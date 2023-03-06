dfeb1dfd838165eb96ebbe6ae713a09669321bba
ReviewEdge <reviewedge56@gmail.com>
2023-03-06 00:14:25 -0500

    set up 2048, creat random player

---
+++ player.py
@@ -0,0 +1,24 @@
+import pyautogui
+
+pyautogui.hotkey('c')
+
+
+
+
+	# while loop will break as the
+	# random cell chosen will be empty
+	# (or contains zero)
+    while True:
+        r = random.randint(0, 3)
+		c = random.randint(0, 3)
+        
+        
+        if mat[r]
+	while(mat[r][c] != 0):
+		r = random.randint(0, 3)
+		c = random.randint(0, 3)
+		
+		mat[r][c] = 2
+
+        # we will place a 2 at that empty
+        # random cell.
---
+++ player.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# source path-to-venv/bin/activate
+XAUTHORITY=/run/user/1000/gdm/Xauthority
+python3 /home/logan/projects/ai2048/player.py
